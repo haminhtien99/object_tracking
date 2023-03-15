cb190e07b78dc3617dfdda3b8ff12e1826ebe85c
Ха Минь Тиен <100370314+haminhtien99@users.noreply.github.com>
2023-03-15 23:10:24 +0300

    add main file

---
+++ single_tracking.py
@@ -0,0 +1,36 @@
+import cv2
+import sys
+from glob import glob
+
+from module import read_frame as rf
+from module import track_frame as track
+
+root  = '/home/ha/Downloads/VisDrone2019-SOT-val/sequences/'
+folders = glob(root +'/*/')
+path = folders[5]
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+if __name__ == '__main__' :
+ 
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[2]
+
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.legacy.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.legacy.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.legacy.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.legacy.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.legacy.TrackerMedianFlow_create()
+        if tracker_type == 'MOSSE':
+            tracker = cv2.legacy.TrackerMOSSE_create()
+        if tracker_type == "CSRT":
+            tracker = cv2.legacy.TrackerCSRT_create()
+    track(path, tracker)
+# rf(path,100)
