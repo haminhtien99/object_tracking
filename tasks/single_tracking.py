580b2cd292657b68127b5c06e45858c3dc4e471a
haminhtien99 <haminhtien747@gmail.com>
2023-08-30 14:13:43 +0300

    update this repository

--- tasks/single_tracking.py
+++ tasks/single_tracking.py
@@ -2,10 +2,21 @@ import cv2
 import sys
 from glob import glob
 
+import os
+import sys
+from pathlib import Path
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+
 from modules.module_1 import read_frame as rf
 from modules.module_1 import track_frame as track
 
-path = 'VisDrone2019-VID-val/sequences/uav0000305_00000_v'
+path = 'C:/Users/ABC/Desktop/VisDrone2019-VID-val/sequences/uav0000305_00000_v'
+
 
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 
