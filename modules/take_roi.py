427cc6dbe18dbc072f47eda6a548e8168261349e
haminhtien99 <haminhtien747@gmail.com>
2024-01-09 17:15:31 +0300

    update

--- modules/take_roi.py
+++ modules/take_roi.py
@@ -1,14 +1,21 @@
 import cv2
 def take_roi(path_to_img):
     img = cv2.imread(path_to_img)
+    imgResize = cv2.resize(img, (1000,1000))
     print(img.shape[0], img.shape[1])
-    bbox = cv2.selectROI(img, False)
-    x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
+    bbox = cv2.selectROI(imgResize, False)
+    # x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
+    x = int(bbox[0] * img.shape[1]/1000)
+    y = int(bbox[1] * img.shape[0]/1000)
+    w= int(bbox[2] * img.shape[1]/1000)
+    h = int(bbox[3] * img.shape[0]/1000)
+    
     roi = img[y: y+h,x:x+w]
     # roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     cv2.destroyAllWindows()
-    return roi, bbox
-path = "C:/Users/ABC/Downloads/images/00000455.jpg"
+    return roi, bbox, x,y,w,h
+
+path = "C:/Users/ABC/Downloads/Telegram Desktop/Xian_station-CN_GoogleSat_v0.tif"
 
 
 
@@ -16,5 +23,7 @@ path = "C:/Users/ABC/Downloads/images/00000455.jpg"
 
 
 
-_, bbox = take_roi(path)
-print(bbox)
+if __name__ == "__main__":
+    _, bbox, x,y,w,h = take_roi(path)
+    print(bbox,x,y,w,h)
+
---
+++ tasks/track_from_webcam.py
@@ -0,0 +1,61 @@
+import cv2
+import os
+import sys
+from pathlib import Path
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+from modules.webcamera import staticROI
+
+
+class track_staticROI(staticROI):
+    def __init__(self):
+        super().__init__()
+        self.tracker = None
+    def update(self, track = None):
+        self.tracker = track
+        if not self.capture.isOpened():
+            print("cannot open the video capture")
+            return
+        running = True
+        tracking = False
+        while running:
+            # Read frame
+            (self.status, self.frame) = self.capture.read()
+            if tracking is False:
+                cv2.imshow('image', self.frame)
+                key = cv2.waitKey(2)
+
+            # Crop image
+            if key == ord('c'):
+                tracking = True
+                self.clone = self.frame.copy()
+                cv2.namedWindow('image')
+                cv2.setMouseCallback('image', self.extract_coordinates)
+                while True:
+                    key = cv2.waitKey(2)
+                    cv2.imshow('image', self.clone)
+                    
+                    if key == ord('\r'):
+                        # Crop and display cropped image
+                        self.crop_ROI()
+                        self.show_cropped_ROI()
+                        print('tracking ....')
+                        # Resume video
+                        break
+            
+            if tracking is True:
+                self.frame = cv2.rectangle(self.frame, self.image_coordinates[0], self.image_coordinates[1], (0,255, 0), 2)
+                cv2.imshow('image', self.frame)
+                key = cv2.waitKey(2)
+            # Close program with keyboard 'q'
+            if key == 27:
+                self.capture.release()
+                cv2.destroyAllWindows()
+                running = False
+                
+                
+track = track_staticROI()
+track.update()
